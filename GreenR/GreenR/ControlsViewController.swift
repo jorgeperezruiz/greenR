f82fbc263ce2f623fb637e53079d5c28ab6dda59
Nikash Yogesh Ramsorrun <specineff00@gmail.com>
2019-08-30 14:13:35 +0100

    Networking hooked up to present data and line chart updating. ControlsVC made (for powering device)

---
+++ GreenR/GreenR/ControlsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ControlsViewController.swift
+//  GreenR
+//
+//  Created by Nikash Ramsorrun on 30/08/2019.
+//  Copyright © 2019 GreenR. All rights reserved.
+//
+
+import UIKit
+
+class ControlsViewController: UIViewController {
+    let network = Networking()
+
+    
+}

c3cd5e99942b7701b1cb81ad83f4ad46fe2dcef9
Jorge Perez Ruiz <akumayken@gmail.com>
2019-08-30 12:25:33 +0100

    Add graph library

--- GreenR/GreenR/ViewController.swift
+++ GreenR/GreenR/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lineChart: LineChartView!
+    
+    
+    var lineChartEntry = [ChartDataEntry]()
+    let data = LineChartData()
+    var counter = 0
+    var line = LineChartDataSet()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for i in counter...10 {
+            let value = ChartDataEntry(x:Double(i), y:Double(i+1))
+            lineChartEntry.append(value)
+        }
+        
+        counter = 10
+        
+        line = LineChartDataSet(entries: lineChartEntry, label: "Kw/h")
+        
+//        lineChart.backgroundColor = UIColor.blue
+        line.valueColors = [UIColor.white]
+        lineChart.drawBordersEnabled = false
+        lineChart.drawGridBackgroundEnabled = false
+        
+        line.circleRadius = 2
+        data.addDataSet(line)
+        lineChart.data = data
     }
 
     

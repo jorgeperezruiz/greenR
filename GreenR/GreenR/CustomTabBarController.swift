3ab0b5ba428c968a56169ba7beb618fb308e4947
Pete Holdsworth <pete.holdsworth@futureplatforms.com>
2019-08-30 14:49:47 +0100

    Change TabBar deselected icon color to blue

---
+++ GreenR/GreenR/CustomTabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTabBarController.swift
+//  GreenR
+//
+//  Created by Pete Holdsworth on 30/08/2019.
+//  Copyright © 2019 GreenR. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBar.unselectedItemTintColor = UIColor(red: 149/255, green: 213/255, blue: 255/255, alpha: 1)
+
+    }
+    
+
+
+}
